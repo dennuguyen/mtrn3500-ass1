74803f5a8788e5019efc7a308cd347ba38c7064f
dennuguyen <dennuguyen@gmail.com>
2020-09-20 17:19:38 +1000

    Add userWait()

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,14 @@
 #include "Galil.h"
 
+static void userWait() {
+    std::cout << "Press ENTER to continue..." << std::endl;
+    std::cin.ignore();
+}
+
 int main(void) {
     EmbeddedFunctions* embf = new EmbeddedFunctions();
-    //GCStringIn addr = "192.168.172.58";
-    //Galil galil(embf, addr);
-    //galil.DigitalBitInput(2);
+    GCStringIn addr = "192.168.172.58";
+    Galil galil(embf, addr);
+    userWait();
     return 0;
 }

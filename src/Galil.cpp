88ec461e3fefe266f0af01010df6bda8463a1464
dennuguyen <dennuguyen@gmail.com>
2020-09-21 19:44:21 +1000

    Associate check() as method of Galil

--- src/Galil.cpp
+++ src/Galil.cpp
@@ -3,97 +3,6 @@
 #include <cstdlib>
 #include <string>
 
-static void check(GReturn code) {
-    return;
-    if (code == G_NO_ERROR)
-        return;
-
-    std::cerr << "GCLIB_ERROR " << code << ": ";
-    switch (code) {
-        case G_NO_ERROR:
-            std::cerr << G_NO_ERROR_S;
-            break;
-        case G_GCLIB_ERROR:
-            std::cerr << G_GCLIB_ERROR_S;
-            break;
-        case G_GCLIB_UTILITY_ERROR:
-            std::cerr << G_GCLIB_UTILITY_ERROR_S;
-            break;
-        case G_GCLIB_UTILITY_IP_TAKEN:
-            std::cerr << G_GCLIB_UTILITY_IP_TAKEN_S;
-            break;
-        case G_GCLIB_NON_BLOCKING_READ_EMPTY:
-            std::cerr << G_GCLIB_NON_BLOCKING_READ_EMPTY;
-            break;
-        case G_TIMEOUT:
-            std::cerr << G_TIMEOUT_S;
-            break;
-        case G_OPEN_ERROR:
-            std::cerr << G_OPEN_ERROR_S;
-            break;
-        case G_READ_ERROR:
-            std::cerr << G_READ_ERROR_S;
-            break;
-        case G_WRITE_ERROR:
-            std::cerr << G_WRITE_ERROR_S;
-            break;
-        case G_INVALID_PREPROCESSOR_OPTIONS:
-            std::cerr << G_INVALID_PREPROCESSOR_OPTIONS_S;
-            break;
-        case G_COMMAND_CALLED_WITH_ILLEGAL_COMMAND:
-            std::cerr << G_COMMAND_CALLED_WITH_ILLEGAL_COMMAND_S;
-            break;
-        case G_DATA_RECORD_ERROR:
-            std::cerr << G_DATA_RECORD_ERROR_S;
-            break;
-        case G_UNSUPPORTED_FUNCTION:
-            std::cerr << G_UNSUPPORTED_FUNCTION_S;
-            break;
-        case G_FIRMWARE_LOAD_NOT_SUPPORTED:
-            std::cerr << G_FIRMWARE_LOAD_NOT_SUPPORTED_S;
-            break;
-        case G_ARRAY_NOT_DIMENSIONED:
-            std::cerr << G_ARRAY_NOT_DIMENSIONED_S;
-            break;
-        case G_ILLEGAL_DATA_IN_PROGRAM:
-            std::cerr << G_ILLEGAL_DATA_IN_PROGRAM_S;
-            break;
-        case G_UNABLE_TO_COMPRESS_PROGRAM_TO_FIT:
-            std::cerr << G_UNABLE_TO_COMPRESS_PROGRAM_TO_FIT_S;
-            break;
-        case G_BAD_RESPONSE_QUESTION_MARK:
-            std::cerr << G_BAD_RESPONSE_QUESTION_MARK_S;
-            break;
-        case G_BAD_VALUE_RANGE:
-            std::cerr << G_BAD_VALUE_RANGE_S;
-            break;
-        case G_BAD_FULL_MEMORY:
-            std::cerr << G_BAD_FULL_MEMORY_S;
-            break;
-        case G_BAD_LOST_DATA:
-            std::cerr << G_BAD_LOST_DATA_S;
-            break;
-        case G_BAD_FILE:
-            std::cerr << G_BAD_FILE_S;
-            break;
-        case G_BAD_ADDRESS:
-            std::cerr << G_BAD_ADDRESS_S;
-            break;
-        case G_BAD_FIRMWARE_LOAD:
-            std::cerr << G_BAD_FIRMWARE_LOAD_S;
-            break;
-        case G_GCAPS_OPEN_ERROR:
-            std::cerr << G_GCAPS_OPEN_ERROR_S;
-            break;
-        case G_GCAPS_SUBSCRIPTION_ERROR:
-            std::cerr << G_GCAPS_SUBSCRIPTION_ERROR_S;
-            break;
-        default:
-            break;
-    }
-    std::cerr << std::endl;
-}
-
 Galil::Galil(EmbeddedFunctions* Funcs, GCStringIn address)
     : Functions(Funcs), g(), ReadBuffer(), ControlParameters(), setPoint() {
     check(Functions->GOpen(address, &g));
@@ -136,7 +45,7 @@ bool Galil::DigitalBitInput(uint8_t bit) {
 }
 
 bool Galil::CheckSuccessfulWrite() {
-    return 0;
+    return LastCode == G_NO_ERROR;
 }
 
 float Galil::AnalogInput(uint8_t channel) {
@@ -170,11 +79,11 @@ std::ostream& operator<<(std::ostream& output, Galil& galil)
 {
     GSize info_len = 100;
     GCStringOut info = new char[info_len];
-    check(galil.Functions->GInfo(galil.g, info, info_len));
+    galil.check(galil.Functions->GInfo(galil.g, info, info_len));
 
     GSize ver_len = 100;
     GCStringOut ver = new char[ver_len];
-    check(galil.Functions->GVersion(ver, ver_len));
+    galil.check(galil.Functions->GVersion(ver, ver_len));
 
     output << info << std::endl << std::endl;
     output << ver << std::endl << std::endl;
@@ -189,4 +98,96 @@ void Galil::call(std::string Command) {
     std::cout << ":" << Command.c_str() << std::endl;
     check(Functions->GCommand(g, Command.c_str(), ReadBuffer, BUFFER_LEN, NULL));
     std::cout << ReadBuffer;
+}
+
+void Galil::check(GReturn code) {
+    LastCode = code;
+
+    if (code == G_NO_ERROR)
+        return;
+
+    std::cerr << "GCLIB_ERROR " << code << ": ";
+    switch (code) {
+    case G_NO_ERROR:
+        std::cerr << G_NO_ERROR_S;
+        break;
+    case G_GCLIB_ERROR:
+        std::cerr << G_GCLIB_ERROR_S;
+        break;
+    case G_GCLIB_UTILITY_ERROR:
+        std::cerr << G_GCLIB_UTILITY_ERROR_S;
+        break;
+    case G_GCLIB_UTILITY_IP_TAKEN:
+        std::cerr << G_GCLIB_UTILITY_IP_TAKEN_S;
+        break;
+    case G_GCLIB_NON_BLOCKING_READ_EMPTY:
+        std::cerr << G_GCLIB_NON_BLOCKING_READ_EMPTY;
+        break;
+    case G_TIMEOUT:
+        std::cerr << G_TIMEOUT_S;
+        break;
+    case G_OPEN_ERROR:
+        std::cerr << G_OPEN_ERROR_S;
+        break;
+    case G_READ_ERROR:
+        std::cerr << G_READ_ERROR_S;
+        break;
+    case G_WRITE_ERROR:
+        std::cerr << G_WRITE_ERROR_S;
+        break;
+    case G_INVALID_PREPROCESSOR_OPTIONS:
+        std::cerr << G_INVALID_PREPROCESSOR_OPTIONS_S;
+        break;
+    case G_COMMAND_CALLED_WITH_ILLEGAL_COMMAND:
+        std::cerr << G_COMMAND_CALLED_WITH_ILLEGAL_COMMAND_S;
+        break;
+    case G_DATA_RECORD_ERROR:
+        std::cerr << G_DATA_RECORD_ERROR_S;
+        break;
+    case G_UNSUPPORTED_FUNCTION:
+        std::cerr << G_UNSUPPORTED_FUNCTION_S;
+        break;
+    case G_FIRMWARE_LOAD_NOT_SUPPORTED:
+        std::cerr << G_FIRMWARE_LOAD_NOT_SUPPORTED_S;
+        break;
+    case G_ARRAY_NOT_DIMENSIONED:
+        std::cerr << G_ARRAY_NOT_DIMENSIONED_S;
+        break;
+    case G_ILLEGAL_DATA_IN_PROGRAM:
+        std::cerr << G_ILLEGAL_DATA_IN_PROGRAM_S;
+        break;
+    case G_UNABLE_TO_COMPRESS_PROGRAM_TO_FIT:
+        std::cerr << G_UNABLE_TO_COMPRESS_PROGRAM_TO_FIT_S;
+        break;
+    case G_BAD_RESPONSE_QUESTION_MARK:
+        std::cerr << G_BAD_RESPONSE_QUESTION_MARK_S;
+        break;
+    case G_BAD_VALUE_RANGE:
+        std::cerr << G_BAD_VALUE_RANGE_S;
+        break;
+    case G_BAD_FULL_MEMORY:
+        std::cerr << G_BAD_FULL_MEMORY_S;
+        break;
+    case G_BAD_LOST_DATA:
+        std::cerr << G_BAD_LOST_DATA_S;
+        break;
+    case G_BAD_FILE:
+        std::cerr << G_BAD_FILE_S;
+        break;
+    case G_BAD_ADDRESS:
+        std::cerr << G_BAD_ADDRESS_S;
+        break;
+    case G_BAD_FIRMWARE_LOAD:
+        std::cerr << G_BAD_FIRMWARE_LOAD_S;
+        break;
+    case G_GCAPS_OPEN_ERROR:
+        std::cerr << G_GCAPS_OPEN_ERROR_S;
+        break;
+    case G_GCAPS_SUBSCRIPTION_ERROR:
+        std::cerr << G_GCAPS_SUBSCRIPTION_ERROR_S;
+        break;
+    default:
+        break;
+    }
+    std::cerr << std::endl;
 }
